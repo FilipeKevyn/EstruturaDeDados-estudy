a909789842e446ac80e04f922d3ca02b12c23b6e
FilipeKevyn <FilipeKevyn4@gmail.com>
2024-10-16 11:32:36 -0300

    Pairs and Factorial methods

--- src/main/java/aula/Recursion.java
+++ src/main/java/aula/Recursion.java
@@ -6,27 +6,51 @@ import java.util.List;
 
 public class Recursion {
 
-    public int findTwo(List<Integer> list){
+    public int countTwo(List<Integer> list){
         if (list.isEmpty()) {
             return 0;
         }
         else {
             if (list.get(0) == 2){
                 list.remove(0);
-                return 1 + findTwo(list);
+                return 1 + countTwo(list);
             }
             else{
                 list.remove(0);
-                return findTwo(list);
+                return countTwo(list);
+            }
+        }
+    }
+
+    public int factorial(int num){
+        if (num == 1){
+            return num;
+        }
+        else {
+            return num * factorial(num - 1);
+        }
+    }
+
+    public static void pares(int num){
+        if (num == 0){
+            System.out.println(0);
+            return;
+        }
+        else {
+            if (num % 2 == 0) {
+                pares(num - 2);
+                System.out.println(num);
+            }
+            else {
+                pares(num - 1);
             }
         }
     }
 
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,2));
         Recursion recursividade = new Recursion();
-        System.out.println(recursividade.findTwo(list));
+        pares(8);
     }
 }
 
