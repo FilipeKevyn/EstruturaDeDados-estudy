726b0155db202f312cc426a2fd747ae5ca633134
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-10-22 14:16:20 -0300

    Insertion Sort

--- src/main/java/sort/InsertionSort.java
+++ src/main/java/sort/InsertionSort.java
@@ -1,2 +1,17 @@
-package sort;public class InsertionSort {
+package sort;
+
+public class InsertionSort {
+    public static int[] sort(int[] vetor){
+         int aux;
+        for (int i = 1; i < vetor.length; i++) {
+            aux = vetor[i];
+            int j = i - 1;
+            while (j > 0 && vetor[j] > aux){
+                vetor[j + 1] = vetor[j];
+                j--;
+            }
+            vetor[j + 1] = aux;
+        }
+        return vetor;
+    }
 }
