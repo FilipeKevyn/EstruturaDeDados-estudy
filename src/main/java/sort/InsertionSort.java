e5c0ce7dfc9f328f518cafac962324f4949665d4
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-10-23 14:06:26 -0300

    refatoring while method

--- src/main/java/sort/InsertionSort.java
+++ src/main/java/sort/InsertionSort.java
@@ -6,12 +6,20 @@ public class InsertionSort {
         for (int i = 1; i < vetor.length; i++) {
             aux = vetor[i];
             int j = i - 1;
-            while (j > 0 && vetor[j] > aux){
+            while (j >= 0 && vetor[j] > aux){
                 vetor[j + 1] = vetor[j];
                 j--;
             }
             vetor[j + 1] = aux;
         }
         return vetor;
+    }
+
+    public static void main(String[] args) {
+        int[] vetor = new int[] {9,5,15,1,2,4};
+        int[] newVetor = sort(vetor);
+        for (int i = 0; i < newVetor.length; i++) {
+            System.out.println(newVetor[i]);
+        };
     }
 }
