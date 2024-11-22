47f1b38fe4aad5565a5acc8196693178b9877c49
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-11-22 10:39:15 -0300

    test: new methods LinkedList

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,16 @@ import list.Node;
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.add("A");
-        list.add("B");
-        list.add("C");
-
+        // adicionar no final
+        list.addEnd("D");
+        list.addEnd("E");
+        list.addEnd("F");
+        // adicionar no começo
+        list.addBeginning("C");
+        list.addBeginning("B");
+        list.addBeginning("A");
+        // adicionar na posição
+        list.add(1,"2");
         System.out.println("Tamanho: " + list.getLength());
         System.out.println("Primeiro: " + list.getFirst().getValue());
         System.out.println("Último: " + list.getLast().getValue());
@@ -16,10 +22,10 @@ public class Main {
         }
         // remover
 
-        list.remove(0);
-        System.out.println("Removeu");
-        for (int i = 0; i < list.getLength(); i++) {
-            System.out.println(list.get(i).getValue());
-        }
+//        list.remove(0);
+//        System.out.println("Removeu");
+//        for (int i = 0; i < list.getLength(); i++) {
+//            System.out.println(list.get(i).getValue());
+//        }
     }
 }
